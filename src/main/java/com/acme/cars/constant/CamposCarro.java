bda9ffe4af2086c5be58b3d686289f760d0105fc
thiagos86 <contato.thiago86@gmail.com>
2026-07-03 09:26:06 -0300

    Sprint 2.2 -> Criação de constante para filtro de carr

---
+++ src/main/java/com/acme/cars/constant/CamposCarro.java
@@ -0,0 +1,11 @@
+package com.acme.cars.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CamposCarro {
+
+    public static final String MOLEDO = "modelo";
+    public static final String FABRICANTE = "fabricante";
+    public static final String PAIS  = "pais";
+}
